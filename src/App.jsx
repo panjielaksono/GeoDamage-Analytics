3fc0b2fdaefeac58f2be72c09c3195045b13ceba
Panji Laksono <120160341+panjielaksono@users.noreply.github.com>
2026-06-08 18:09:31 +0700

    benerin dikit rahasia

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ import nonDamageImg from "./assets/nonDamage.png";
 import waterImg from "./assets/water.png";
 import rocketIcon from "./assets/iconroket.png";
 
-const HF_SPACE_API = "https://breaad-disaster-analytics-api.hf.space/predict";
+const HF_SPACE_API = "import.meta.env.VITE_HF_SPACE_API";
 const SHOWCASE_CLASSES = [
   {
     name: "Damaged Infrastructure",
